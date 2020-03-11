1eafd7d0c98f43a799adeb94b42867d4092802f9
unknown <chris_palma@mail.fresnostate.edu>
2020-03-11 15:17:22 -0700

    finished lab 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ArtworkComponent } from './artwork/artwork.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ArtistComponent } from './artist/artist.component';
+import { routerModule } from './routes';
+
+import { SharedComponentsModule } from './shared-components/shared-components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ArtworkComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ArtistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routerModule,
+    SharedComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
